5262b9a26f2243cf40db54a6b7b3108b9fc3dffd
unknown <Antonio@Antonio.(none)>
2012-02-27 08:53:42 +0100

    Fusionado con el resto de ramas, SQLite y master

--- src/Cobo.py
+++ src/Cobo.py
@@ -1,20 +1,4 @@
 # -*- coding: cp1252 -*-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-# TODO: Borrar acciones que pertenezcan al mercado PCX y borrar indice que los contega para evitar que se vuelvan a anadir automaticamente
-
->>>>>>> SQLite
-
-############################################################
-# modulos estandar importados
-=======
-# TODO: Borrar acciones que pertenezcan al mercado PCX y borrar indice que los contega para evitar que se vuelvan a anadir automaticamente
-
-
-############################################################
-# módulos estándar importados
->>>>>>> master
 
 #import urllib
 import urllib2
